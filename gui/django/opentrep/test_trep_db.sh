49725a6c25d7391294d7437c8a0c14a8a75503ea
Denis Arnaud <denis.arnaud_fedora@m4x.org>
2011-12-26 15:40:16 +0100

    [WebApps] Created the 'webapps' sub-branch.

--- gui/django/opentrep/test_trep_db.sh
+++
@@ -1,7 +0,0 @@
-
-# Do not execute this file. Source it instead
-CURRENT_DIR=`pwd`
-DJANGO_BASE_DIR=`dirname ${CURRENT_DIR}`
-export DJANGO_SETTINGS_MODULE=opentrep.settings
-export PYTHONPATH=${PYTHONPATH}:${DJANGO_BASE_DIR}
-
